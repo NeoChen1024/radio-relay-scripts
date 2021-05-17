c4e632007309a16c2cb4ed6164399029582c7149
Neo_Chen <chenkolei@gmail.com>
2021-05-17 08:40:19 +0800

    up

--- freq_sched.sh
+++ freq_sched.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env zsh
-
 alias ctl="rigctl -m 4"
 
 time="$(date +"%H%M%S")"
 echo "TIME=${time}"
+
 if grep "$time" schedule.list > /ram/sch.list; then
 	freq="$(cut -f 2 -d $'\t' < /ram/sch.list)"
 	mode="$(cut -f 3 -d $'\t' < /ram/sch.list)"
 	bw="$(cut -f 4 -d $'\t' < /ram/sch.list)"
 	
-	ctl F "${freq}"
-	ctl M "$mode" "$bw"
+	echo "$time: $freq, $mode, $bw" >> /ram/set.txt
+	ctrl V VFOA
+	ctl F "${freq}" M "$mode" "$bw" V VFOA
 fi
+
+ctl f m
