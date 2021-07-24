c859f8b9b078dbf9eebd76b09a9ce2e1b8bbf8c3
Neo_Chen <chenkolei@gmail.com>
2021-07-24 23:40:27 +0800

    up

--- freq_sched.sh
+++ freq_sched.sh
@@ -4,7 +4,7 @@ alias ctl="rigctl -m 4"
 time="$(date +"%H%M%S")"
 echo "TIME=${time}"
 
-if grep "$time" schedule.list > /ram/sch.list; then
+if grep "^$time" schedule.list > /ram/sch.list; then
 	freq="$(cut -f 2 -d $'\t' < /ram/sch.list)"
 	mode="$(cut -f 3 -d $'\t' < /ram/sch.list)"
 	bw="$(cut -f 4 -d $'\t' < /ram/sch.list)"
